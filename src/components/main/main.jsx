50cd0601fa0fea1f730553dd548e301377346709
bremxx <petr.kirienko@gmail.com>
2022-04-04 16:23:34 +0300

    selectors updated, removed unused memoization

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -1,37 +1,33 @@
 import {nanoid} from "nanoid";
-import React, {useCallback, useMemo, useState} from "react";
+import React, {useCallback, useState} from "react";
 import {useDispatch, useSelector} from "react-redux";
 
 import {ID_LENGTH} from "../../const";
 import {debounce} from "../../utils";
 import {deleteAllMessages, postNewMessage} from "../../store/action";
-import {makeGetUnseenMessagesNumSelector} from "../../store/selectors";
+import {getUnseenMessagesNumSelector} from "../../store/selectors";
 import Controls from "../controls/controls";
 import MessagesList from "../messages-list/messages-list";
 
-
 const Main = () => {
-
-  const getUnseenMessagesNum = useMemo(makeGetUnseenMessagesNumSelector, []);
-  const unseenMessagesNum = useSelector((state) => getUnseenMessagesNum(state));
+  const unseenMessagesNum = useSelector((state) =>
+    getUnseenMessagesNumSelector(state)
+  );
 
   const dispatch = useDispatch();
 
   const [isListVisible, setIsListVisible] = useState(false);
   const [isFullListShown, setIsFullListShown] = useState(false);
 
-  const handleSubmit = useCallback(
-      (newMessageText) => {
-        const newMessage = {
-          id: nanoid(ID_LENGTH),
-          text: newMessageText,
-          seen: isListVisible,
-          createdAt: new Date().toISOString()
-        };
-        dispatch(postNewMessage(newMessage));
-      },
-      []
-  );
+  const handleSubmit = useCallback((newMessageText) => {
+    const newMessage = {
+      id: nanoid(ID_LENGTH),
+      text: newMessageText,
+      seen: isListVisible,
+      createdAt: new Date().toISOString(),
+    };
+    dispatch(postNewMessage(newMessage));
+  }, []);
 
   const handleDeleteAll = () => {
     dispatch(deleteAllMessages());
@@ -40,15 +36,15 @@ const Main = () => {
       setIsFullListShown(false);
     }
   };
-  const debouncedHandleDeleteAll = useCallback(debounce(() => handleDeleteAll()), []);
-
-  const handlePopupToggleClick = useCallback(
-      () => {
-        setIsListVisible(!isListVisible);
-        setIsFullListShown(false);
-      }, [isListVisible, isFullListShown]
+  const debouncedHandleDeleteAll = useCallback(
+      debounce(() => handleDeleteAll()),
+      []
   );
 
+  const handlePopupToggleClick = useCallback(() => {
+    setIsListVisible(!isListVisible);
+    setIsFullListShown(false);
+  }, [isListVisible, isFullListShown]);
 
   return (
     <div className="main-container">
@@ -56,8 +52,11 @@ const Main = () => {
         <div className="board__header header">
           <div
             className="header__icon"
-            onClick={() => handlePopupToggleClick()}>
-            { (unseenMessagesNum > 0) && <div className="header__icon-indicator">{unseenMessagesNum}</div> }
+            onClick={() => handlePopupToggleClick()}
+          >
+            {unseenMessagesNum > 0 && (
+              <div className="header__icon-indicator">{unseenMessagesNum}</div>
+            )}
           </div>
         </div>
 
@@ -68,13 +67,12 @@ const Main = () => {
             handlePopupToggleClick={handlePopupToggleClick}
           />
 
-          {
-            isListVisible &&
+          {isListVisible && (
             <MessagesList
               isFullListShown={isFullListShown}
               setIsFullListShown={setIsFullListShown}
             />
-          }
+          )}
         </div>
       </div>
     </div>
--- src/components/messages-list/messages-list.jsx
+++ src/components/messages-list/messages-list.jsx
@@ -1,62 +1,59 @@
-import React, {useEffect, useMemo} from "react";
+import React, {useEffect} from "react";
 import PropTypes from "prop-types";
 import {useDispatch, useSelector} from "react-redux";
 import {changeMessageSeenStatus} from "../../store/action";
-import {makeGetMessagesSelector} from "../../store/selectors";
+import {getMessagesSelector} from "../../store/selectors";
 import {humanizeDate} from "../../utils";
 
 const MessagesList = ({isFullListShown, setIsFullListShown}) => {
-
-  const getMessages = useMemo(makeGetMessagesSelector, []);
-  const messages = useSelector((state) => getMessages(state));
+  const messages = useSelector((state) => getMessagesSelector(state));
 
   const visibleMessagesNum = useSelector((state) => state.visibleMessagesNum);
 
-  const getVisibleMessages = () => (
-    isFullListShown
-      ? messages
-      : messages.slice(0, visibleMessagesNum)
-  );
+  const getVisibleMessages = () =>
+    isFullListShown ? messages : messages.slice(0, visibleMessagesNum);
 
   const messagesToShow = getVisibleMessages();
 
   const dispatch = useDispatch();
 
-  useEffect(() =>
-    messagesToShow.forEach((item) => {
-      if (!item.seen) {
-        dispatch(changeMessageSeenStatus(item.id, true));
-      }
-    }), [isFullListShown, messages]
+  useEffect(
+      () =>
+        messagesToShow.forEach((item) => {
+          if (!item.seen) {
+            dispatch(changeMessageSeenStatus(item.id, true));
+          }
+        }),
+      [isFullListShown, messages]
   );
 
   return (
     <div className="board__list-container ">
       <ul className="board__list msg-list">
-        {
-          messagesToShow.map(
-              (item, i) =>
-                <li className="msg-list__item msg" key={`msg-${i}`}>
-                  <div className="msg__title">{item.text}</div>
-                  <div className="msg__time">{humanizeDate(`D MMM, HH:mm:ss`, item.createdAt)}</div>
-                </li>
-          )
-        }
+        {messagesToShow.map((item, i) => (
+          <li className="msg-list__item msg" key={`msg-${i}`}>
+            <div className="msg__title">{item.text}</div>
+            <div className="msg__time">
+              {humanizeDate(`D MMM, HH:mm:ss`, item.createdAt)}
+            </div>
+          </li>
+        ))}
       </ul>
-      {
-        (messages.length > visibleMessagesNum && !isFullListShown) &&
+      {messages.length > visibleMessagesNum && !isFullListShown && (
         <span
           className="msg-list__link"
           onClick={() => setIsFullListShown(true)}
-        >Посмотреть все...</span>
-      }
+        >
+          Посмотреть все...
+        </span>
+      )}
     </div>
   );
 };
 
 MessagesList.propTypes = {
   isFullListShown: PropTypes.bool.isRequired,
-  setIsFullListShown: PropTypes.func.isRequired
+  setIsFullListShown: PropTypes.func.isRequired,
 };
 
 export default MessagesList;
