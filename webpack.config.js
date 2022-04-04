b57c42ad91dda57fb279d30b5a07556a72b31223
bremxx <petr.kirienko@gmail.com>
2022-04-04 16:21:04 +0300

    webpack settings edited, scripts updated

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,49 @@
-const path = require('path');
+const path = require("path");
+const TerserPlugin = require("terser-webpack-plugin");
+
+const isDevMode = process.env.NODE_ENV === `development`;
+const isProdMode = process.env.NODE_ENV === `production`;
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'public')
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "public"),
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "public"),
     },
-    devServer: {
-        contentBase: path.resolve(__dirname, 'public'),
-        open: true,
-        port: 1333,
-        historyApiFallback: true,
-    },
-    module: {
-        rules: [
-        {
-            test: /\.(js|jsx)$/,
-            exclude: /node_modules/,
-            use: {
-            loader: 'babel-loader',
-            },
-        }
-        ],
-    },
-    resolve: {
-        extensions: ['.js', '.jsx']
-    },
-    devtool: 'source-map',
+    open: true,
+    port: isDevMode ? 8080 : 1333,
+    historyApiFallback: true,
+  },
+  optimization: {
+    minimize: isProdMode,
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          format: {
+            comments: false,
+          },
+        },
+        extractComments: false,
+      }),
+    ],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  devtool: "source-map",
 };
